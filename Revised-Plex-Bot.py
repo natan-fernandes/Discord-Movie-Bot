226b926c0d2e4fc66f84c9eefa39360b996b997a
Natan <natanfernandes@protonmail.com>
2024-04-05 00:20:58 -0300

    aa

--- Revised-Plex-Bot.py
+++ Revised-Plex-Bot.py
@@ -70,22 +70,21 @@ p = 0
 r = 0
 
 
-@slash.slash(name="Help", description="Pulls the help menu.")
-async def help(ctx):
-    embed = discord.Embed(title="Help", description=" ", color=0xf5dd03)
-    embed.add_field(name="``/search <keyword>``", value="```fix\nUse this command to lookup a movies that has the keyword mentioned in the title.```", inline=False)
-    embed.add_field(name="``/info <movie>``", value="```fix\nUse this command to get information about a movie like ratings, summary and duration.```", inline=False)
-    embed.add_field(name="``/play <movie>``",value="```fix\nUse this command to play a movie (best to use search first then copy the movie title in the movie field).```",inline=False)
-    embed.add_field(name="``/shuffle``",value="```fix\nUse this command if your not sure what to watch. Picks a random movie and plays it.```",inline=False)
-    embed.add_field(name="``/pause``",value="```fix\nUse this command to pause the currently playing movie.```",inline=False)
-    embed.add_field(name="``/resume``", value="```fix\nUse this command to resume the previously paused movie.```", inline=False)
-    embed.add_field(name="``/stop``", value="```fix\nUse this command to stop the current movie from playing.```", inline=False)
-    embed.set_footer(text="<keyword> = a word inside the title. \n⠀e.g., /search bob gets every movie with the word 'bob' in the title\n<movie> = movie title. \n⠀e.g., /play Step Brothers. (punctuation  matters)")
+@slash.slash(name="Ajuda", description="Mostra o menu de ajuda.")
+async def ajuda(ctx):
+    embed = discord.Embed(title="Ajuda", description=" ", color=0xf5dd03)
+    embed.add_field(name="``/pesquisar <pesquisa>``", value="```fix\nPesquisa um filme.```", inline=False)
+    embed.add_field(name="``/info <filme>``", value="```fix\nObtém informações de um filme.```", inline=False)
+    embed.add_field(name="``/assistir <filme>``",value="```fix\nComeçar a assistir um filme.```",inline=False)
+    embed.add_field(name="``/aleatorio``",value="```fix\nAssistir um filme aleatório.```",inline=False)
+    embed.add_field(name="``/pausar``",value="```fix\nPausa o filme atual.```",inline=False)
+    embed.add_field(name="``/despausar``", value="```fix\nDespausa o filme atual.```", inline=False)
+    embed.add_field(name="``/parar``", value="```fix\nPara o filme atual.```", inline=False)
     await ctx.send(embed=embed)
 
 
-@slash.slash(name="Search", description="Search a movie with a keyword.")
-async def search(ctx, *, keyword):
+@slash.slash(name="Pesquisar", description="Pesquisa um filme.")
+async def search(ctx, *, pesquisa):
     movie = []
     movies = plex.library.section('Movies')
     try:
@@ -93,18 +92,18 @@ async def search(ctx, *, keyword):
             movie.append('%s' % (video.title))
         results = ('\n •'.join(movie))
         if movie == []:
-            results = "No Movies Found!"
-        embed = discord.Embed(title=f"__Search Results:__", description=f"•{results}", color=0xf5dd03)
-        embed.set_footer(text=f"Keyword: {keyword}")
+            results = "Nenhum filme encontrado!"
+        embed = discord.Embed(title=f"__Resultados da pesquisa:__", description=f"•{results}", color=0xf5dd03)
+        embed.set_footer(text=f"Pesquisa: {keyword}")
         await ctx.send(embed=embed)
     except:
-        await ctx.send("Error!!")
+        await ctx.send("Erro!!")
 
 
-@slash.slash(name="Info", description="Get information about a movie.")
-async def info(ctx, *, movie):
+@slash.slash(name="Info", description="Obtém informações de um filme.")
+async def info(ctx, *, filme):
     try:
-        play = plex.library.section('Movies').get(movie)
+        play = plex.library.section('Movies').get(filme)
         client = plex.client(system)
         client.proxyThroughServer()
         image = PosterUrlMixin.thumbUrl.fget(play)
@@ -112,23 +111,23 @@ async def info(ctx, *, movie):
         with open('movie.jpg', 'wb') as handler:
             handler.write(img_data)
         duration = int(play.duration / 60000)
-        embed = discord.Embed(title=f"Infor For: {movie}", description=f"{play.summary}\n\n**Rotten Tomatoes Rating:** {play.audienceRating}\n**Content Rating:** {play.contentRating}\n**Duration:** {duration} Minutes", color=0xf5dd03)
+        embed = discord.Embed(title=f"Infor For: {filme}", description=f"{play.summary}\n\n**Classificação Rotten Tomatoes:** {play.audienceRating}\n**Classificação de conteúdo:** {play.contentRating}\n**Duração:** {duration} minutos", color=0xf5dd03)
         file = discord.File("C:/Users\conta\Desktop\Bots\PlexBot\movie.jpg", filename="movie.jpg")
         embed.set_image(url="attachment://movie.jpg")
         embed.set_footer(text=f"{play.year} - {play.studio}")
         await ctx.send(file=file, embed=embed)
     except:
-        embed = discord.Embed(title=f"I couldn't find: {movie}.", description="If you're having trouble, use /search to search for the movie then copy and paste (punctuation matters).", color=0xf5dd03)
+        embed = discord.Embed(title=f"I couldn't find: {filme}.", description="Filme não encontrado. Use o /pesquisar para obter o nome correto do filme.", color=0xf5dd03)
         await ctx.send(embed=embed)
 
 
-@slash.slash(name="Play", description="Play a movie.")
-async def play(ctx, *, movie):
+@slash.slash(name="assistir", description="Assiste um filme.")
+async def assistir(ctx, *, filme):
     try:
         global currentlyplaying
         global savemovietitle
-        savemovietitle=movie
-        play = plex.library.section('Movies').get(movie)
+        savemovietitle=filme
+        play = plex.library.section('Movies').get(filme)
         client = plex.client(system)
         client.proxyThroughServer()
         client.playMedia(play)
@@ -138,63 +137,63 @@ async def play(ctx, *, movie):
         with open('movie.jpg', 'wb') as handler:
             handler.write(img_data)
         duration = int(play.duration / 60000)
-        embed = discord.Embed(title=f"Playing: {movie}", description=f"{play.summary}\n\n**Rotten Tomatoes Rating:** {play.audienceRating}\n**Content Rating:** {play.contentRating}\n**Duration:** {duration} Minutes", color=0xf5dd03)
+        embed = discord.Embed(title=f"Assistindo: {filme}", description=f"{play.summary}\n\n**Classificação Rotten Tomatoes:** {play.audienceRating}\n**Classificação de conteúdo:** {play.contentRating}\n**Duração:** {duration} minutos", color=0xf5dd03)
         file = discord.File("C:/Users\conta\Desktop\Bots\PlexBot\movie.jpg", filename="movie.jpg")
         embed.set_image(url="attachment://movie.jpg")
         embed.set_footer(text=f"{play.year} - {play.studio}")
         await ctx.send(file=file, embed=embed)
     except:
-        embed = discord.Embed(title=f"I couldn't find: {movie}.", description="If you're having trouble, use /search to search for the movie then copy and paste (punctuation matters).", color=0xf5dd03)
+        embed = discord.Embed(title=f"I couldn't find: {filme}.", description="If you're having trouble, use /search to search for the movie then copy and paste (punctuation matters).", color=0xf5dd03)
         await ctx.send(embed=embed)
 
 
-@slash.slash(name="Stop", description="Stop the current movie.")
-async def stop(ctx):
+@slash.slash(name="Parar", description="Para o filme atual.")
+async def parar(ctx):
     global savemovietitle
     currentlyplaying = plex.sessions()
     if currentlyplaying == []:
-        embed = discord.Embed(title=f"Ops!", description="Nothing currently playing.", color=0xf5dd03)
+        embed = discord.Embed(title=f"Ops!", description="Nenhum filme está sendo exibido.", color=0xf5dd03)
         await ctx.send(embed=embed)
     else:
         client = plex.client(system)
         client.proxyThroughServer()
         client.stop()
-        embed = discord.Embed(title=f"__Stopped Playing__:", description=f"{savemovietitle}", color=0xf5dd03)
+        embed = discord.Embed(title=f"__Parou de tocar__:", description=f"{savemovietitle}", color=0xf5dd03)
         await ctx.send(embed=embed)
 
 
-@slash.slash(name="Pause", description="Pause the currently movie.")
-async def pause(ctx):
+@slash.slash(name="Pausar", description="Pausa o filme atual.")
+async def pausar(ctx):
     global savemovietitle
     currentlyplaying = plex.sessions()
     if p==1:
-        embed = discord.Embed(title=f"Ops!", description=f"{savemovietitle} is already paused.", color=0xf5dd03)
+        embed = discord.Embed(title=f"Ops!", description=f"{savemovietitle} já está pausado.", color=0xf5dd03)
         await ctx.send(embed=embed)
     else:
         client = plex.client(system)
         client.proxyThroughServer()
         client.pause()
-        embed = discord.Embed(title=f"__Paused__:", description=f"{savemovietitle}", color=0xf5dd03)
+        embed = discord.Embed(title=f"__Pausado__:", description=f"{savemovietitle}", color=0xf5dd03)
         await ctx.send(embed=embed)
 
 
-@slash.slash(name="Resume", description="Resume the previously played movie.")
-async def resume(ctx):
+@slash.slash(name="Despausar", description="Despausa o filme.")
+async def despausar(ctx):
     global savemovietitle
     currentlyplaying = plex.sessions()
     if r==1:
-        embed = discord.Embed(title=f"Ops!", description=f"{savemovietitle} is already playing.", color=0xf5dd03)
+        embed = discord.Embed(title=f"Ops!", description=f"{savemovietitle} já está tocando.", color=0xf5dd03)
         await ctx.send(embed=embed)
     else:
         client = plex.client(system)
         client.proxyThroughServer()
         client.play()
-        embed = discord.Embed(title=f"__Resumed__:", description=f"{savemovietitle}", color=0xf5dd03)
+        embed = discord.Embed(title=f"__Despausado__:", description=f"{savemovietitle}", color=0xf5dd03)
         await ctx.send(embed=embed)
 
 
-@slash.slash(name="Shuffle", description="Play a random movie. Please wait...")
-async def shuffle(ctx):
+@slash.slash(name="Aleatorio", description="Toca um filme aleatório. Por favor espere...")
+async def aleatorio(ctx):
     global currentlyplaying
     global savemovietitle
     rc = random.choice(movielist)
@@ -209,7 +208,7 @@ async def shuffle(ctx):
     with open('movie.jpg', 'wb') as handler:
         handler.write(img_data)
     duration = int(play.duration / 60000)
-    embed = discord.Embed(title=f"**Playing:** {rc}", description=f"{play.summary}\n\n**Rotten Tomatoes Rating:** {play.audienceRating}\n**Content Rating:** {play.contentRating}\n**Duration:** {duration} Minutes" ,color=0xf5dd03)
+    embed = discord.Embed(title=f"**Assistindo:** {rc}", description=f"{play.summary}\n\n**Classificação Rotten Tomatoes:** {play.audienceRating}\n**Classificação de conteúdo:** {play.contentRating}\n**Duração:** {duration} minutos" ,color=0xf5dd03)
     file = discord.File("C:/Users\conta\Desktop\Bots\PlexBot\movie.jpg", filename="movie.jpg")
     embed.set_image(url="attachment://movie.jpg")
     embed.set_footer(text=f"{play.year} - {play.studio}")
